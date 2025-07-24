8286c4e900cc9fa058ddf42fb3cf1222c374f462
Emilia Baez Albe <emiliabaezalbe@localhost.localdomain>
2025-07-24 14:15:04 -0300

    Creacion de las dos paginas, estrucutra BASICA, titulo en ambas, boton en pag1

---
+++ src/paginas/Pantalla1.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom';
+
+export default function Pantalla1(){
+    return(
+        <div>
+            <h1> Esta es la pantalla 1</h1>
+            <Link to="/Pantalla2">
+                <button>Pantalla 2</button>
+            </Link>
+        </div>
+    );
+}
---
+++ src/paginas/Pantalla2.jsx
@@ -0,0 +1,9 @@
+// import {link} from 'react-router-dom';
+
+export default function Pantalla2(){
+    return(
+        <div>
+            <h1> Esta es la pantalla 2</h1>
+        </div>
+    );
+}
